1ab47b0784b6d36d4b9556d3f303f37e854987d8
macrocosme <vohl@astron.nl>
2019-03-07 09:36:59 +0000

    Add yaml reader

--- amber_meta/amber_utils.py
+++ amber_meta/amber_utils.py
@@ -1,5 +1,6 @@
 from __future__ import division, print_function
 import os
+import yaml
 
 def pretty_print_command (command):
     c = ''
@@ -42,8 +43,14 @@ def check_directory_exists(directory):
         os.makedirs(directory)
     return directory
 
-def parse_scenario_to_dictionary(scenario_file):
+def parse_sh_scenario_to_dictionary(scenario_file):
     '''Parse an amber scenario file to a python dictionary
+
+    File extension expected is '.sh' as described in
+    https://github.com/AA-ALERT/AMBER_setup/blob/development/examples/scenario.sh
+
+    Note that the extension is not required per se,
+    but the file structure should follow a shell variable structure.
 
     Parameters
     ----------
@@ -60,3 +67,25 @@ def parse_scenario_to_dictionary(scenario_file):
                 param, value = line.replace('\n', '').replace('"', '').split('=')
                 scenario_dict[param] = value
     return scenario_dict
+
+def parse_yaml_scenario_to_dictionary(scenario_file, scenario_name=None):
+    '''Parse an amber scenario file (yaml) to a python dictionary
+
+    Parameters
+    ----------
+        scenario_file: amber scenario file in yaml format (including path)
+
+    Returns
+    -------
+        scenario_dict: parsed dictionary
+    '''
+    scenario_dict = {}
+
+    with file('document.yaml', 'r') as f:
+        scenario_dict = yaml.load(f)
+
+    if scenario_name != None:
+        # e.g. scenario_dict['subband'] for multilayer yaml scenario file
+        return scenario_dict[scenario_name]
+    else:
+        return scenario_dict
