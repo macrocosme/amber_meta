ba507d9cebe9fd092af1ee810118908ef17b94b2
macrocosme <vohl@astron.nl>
2019-03-07 13:24:26 +0000

    Reading amber output to df

--- amber_meta/amber_results.py
+++ amber_meta/amber_results.py
@@ -5,28 +5,44 @@ def get_header(filename, sep=' '):
     with open(filename, 'r') as f:
         return f.readline().split('\n')[0].split('# ')[1].split(sep)
 
-def read_amber_run_results(run_output_dir, verbose=False):
-    sep = ' '
-    dfs = []
-    for file in get_Files(run_output_dir):
+def read_amber_run_results(run_output_dir, verbose=False, sep = ' '):
+    # Get input file's header (amber format)
+    header = get_header(
+        "%s%s" % (
+            check_path_ends_with_slash(run_output_dir),
+            get_Files(run_output_dir)[0]
+        ), sep)
+
+    # Create empty dataframe
+    df = pandas__read_csv(
+        # path/to/file.trigger
+        "%s%s" % (
+            check_path_ends_with_slash(run_output_dir),
+            get_Files(run_output_dir)[0]
+        ),
+        sep=sep,
+        names=header,
+        skiprows=1
+    )
+
+    # Go through output files
+    for file in get_Files(run_output_dir)[1:]:
         if verbose:
             print (file)
 
-        # Get input file's header (amber format)
-        header = get_header(file, sep)
-
-        # Read .trigger file to pandas' dataframe
-        dfs.append(
-            pandas__readcsv(
+        # Append .trigger files to pandas' dataframe
+        df.append(
+            pandas__read_csv(
                 # path/to/file.trigger
                 "%s%s" % (
                     check_path_ends_with_slash(run_output_dir),
-                    filename
+                    file
                 ),
                 sep=sep,
                 names=header,
                 skiprows=1
-            )
+            ),
+            ignore_index=True
         )
 
-    return dfs
+    return df
