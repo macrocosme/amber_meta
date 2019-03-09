5e16bec041af31e86a85884fb768d28aa6f7562b
macrocosme <vohl@astron.nl>
2019-03-09 07:57:07 +0100

    Build docs with readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../../amber_meta'))
 sys.setrecursionlimit(1500)
 
@@ -77,7 +78,9 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+"sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -87,16 +90,6 @@ html_theme_options = {
 #    'logo': 'logo.png',
     'github_user': 'macrocosme',
     'github_repo': 'amber_meta',
-}
-
-html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        # 'relations.html',
-        'searchbox.html',
-        # 'donate.html',
-    ]
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
