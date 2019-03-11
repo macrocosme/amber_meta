699b7a467ab208957cf3de4306d5d3f97385b152
D Vohl <vohl@astron.nl>
2019-03-11 09:19:04 +0100

    Add docstrings for class

--- amber_meta/amber_options.py
+++ amber_meta/amber_options.py
@@ -9,6 +9,62 @@
 """
 
 class AmberOptions:
+    """Class representing amber's command line options.
+
+    The class can be instanciated using default values, or by passing
+    parameters as input. All command options will be availble via
+    self.options.
+
+    >>> amber_options = AmberOptions(rfim=True,
+                                     rfim_mode='time_domain_sigma_cut',
+                                     snr_mode='snr_mom_sigmacut',
+                                     input_data_mode='sigproc',
+                                     downsampling=False
+    >>> amber_options.options
+
+    Parameters
+    ----------
+    rfim : bool (optional)
+        Default: True
+    rfim_mode : str (optional)
+        RFIm mode of operation. Default: 'time_domain_sigma_cut'
+    snr_mode : str (optional)
+        SNR mode of operation. Default: 'snr_mom_sigmacut'
+    input_data_mode : str (optional)
+        Input data mode (sigproc's filterbank file or dada ringbuffer). Default: 'sigproc'
+    downsampling : bool (optional)
+        Enable downsampling. Default: False
+
+    Attributes
+    ----------
+        options_base : list
+            List of basic options
+        options_tdsc : list
+            List of options for RFIm's time domain sigma cut
+        options_fdsc : list
+            List of options for RFIm's frequency domain sigma cut
+        options_rfim : dict
+            Options to choose between RFIm modes
+        options_snr_standard : list
+            List of options for SNR standard
+        options_snr_momad : list
+            List of options for SNR median of medians maximum absolute deviation
+        options_snr_mom_sigmacut : list
+            List of options for SNR median of medians sigma cut
+        options_SNR : dict
+            Options to choose between SNR modes
+        options_downsampling : list
+            List of options for downsampling
+        options_subband_dedispersion : list
+            List of options for subband dedispersion
+        options_sigproc : list
+            List of options for sigproc data input
+        options_dada : list
+            List of options for dada ringbuffer input
+        options_input_data : dict
+            Options to choose between input data modes
+
+    """
     # Base options
     options_base = ['print', 'opencl_platform', 'opencl_device', 'device_name', 'sync', 'padding_file', 'zapped_channels', 'integration_steps', 'integration_file', 'compact_results', 'output', 'dms', 'dm_first', 'dm_step', 'threshold',] #'debug',
 
