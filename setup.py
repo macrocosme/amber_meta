eccd8ec17f6a5fa11a54fac93113385365616048
D Vohl <vohl@astron.nl>
2019-03-12 09:17:02 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='amber_meta',
+      version='0.1dev',
+      packages=['amber_meta',],
+      license='GNU GPL v3+',
+      long_description=open('README.md').read(),
+     )
